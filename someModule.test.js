c598beb4183a6710ce009b119af33119b92a0502
Riccardo Zanetti <znt.riccardo@gmail.com>
2018-10-31 13:44:41 +0100

    add test module + test

---
+++ someModule.test.js
@@ -0,0 +1,10 @@
+const conc = require('./someModule').conc
+
+test('concat test',() => {
+    expect(conc('a','b')).toBe('ab');
+});
+
+test('concat null',() => {
+    expect(conc(null,null)).toBe(0);
+});
+
